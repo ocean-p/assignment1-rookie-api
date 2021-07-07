9764fd130d700f942144afeb8703c0817f12cec2
ocean-p <duongkodoiqua@gmail.com>
2021-07-07 21:45:02 +0700

    API manage accounts - get all, add, update, delete

--- src/main/java/com/daiduong/demo/controller/AdminController.java
+++ src/main/java/com/daiduong/demo/controller/AdminController.java
@@ -2,8 +2,10 @@ package com.daiduong.demo.controller;
 
 import java.util.List;
 
+import com.daiduong.demo.entity.AccountEntity;
 import com.daiduong.demo.entity.CategoryEntity;
 import com.daiduong.demo.entity.ProductEntity;
+import com.daiduong.demo.service.AccountService;
 import com.daiduong.demo.service.CategoryService;
 import com.daiduong.demo.service.ProductService;
 
@@ -14,10 +16,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HomeController {
+@RequestMapping("/admin")
+public class AdminController {
     
     @Autowired
     private CategoryService categoryService;
@@ -25,51 +29,78 @@ public class HomeController {
     @Autowired
     private ProductService productService;
 
+    @Autowired
+    private AccountService accountService;
+
     /** BEGIN: CATEGORY */
-    @GetMapping("/admin/category")
+    @GetMapping("/category")
     public List<CategoryEntity> getAllCategories() {
         return categoryService.getAllCategories();
     }
 
-    @PostMapping("/admin/category/new")
+    @PostMapping("/category/new")
     public CategoryEntity addCategory(@RequestBody CategoryEntity entity) {
         return categoryService.addCategory(entity);
     }
 
-    @PutMapping("/admin/category/new/{id}")
+    @PutMapping("/category/new/{id}")
     public CategoryEntity updateCategory(@PathVariable("id") int id,
                                         @RequestBody CategoryEntity entity)
     {
         return categoryService.updateCategory(id, entity);
     }
     
-    @DeleteMapping("/admin/category/deletion/{id}")
+    @DeleteMapping("/category/deletion/{id}")
     public CategoryEntity deleteCategory(@PathVariable("id") int id){
         return categoryService.deleteCategory(id);
     }
     /** END: CATEGORY */
 
     /** BEGIN: PRODUCT */
-    @PostMapping("/admin/product/new")
+    @PostMapping("/product/new")
     public ProductEntity addProduct(@RequestBody ProductEntity entity){
         return productService.addProduct(entity);
     }
 
-    @GetMapping("/admin/product")
+    @GetMapping("/product")
     public List<ProductEntity> getAllProducts(){
         return productService.getAllProducts();
     }
 
-    @PutMapping("/admin/product/new/{id}")
+    @PutMapping("/product/new/{id}")
     public ProductEntity updateProduct(@PathVariable("id") int id, 
                                        @RequestBody ProductEntity entity)
     {
         return productService.updateProduct(id, entity);
     }
 
-    @DeleteMapping("/admin/product/deletion/{id}")
+    @DeleteMapping("/product/deletion/{id}")
     public ProductEntity deleteProduct(@PathVariable("id") int id){
         return productService.deleteProduct(id);
     }
     /** END: PRODUCT */
+
+    /** BEGIN: ACCOUNT */
+    @PostMapping("/account/new")
+    public AccountEntity addAccount(@RequestBody AccountEntity entity){
+        return accountService.addAccount(entity);
+    }
+
+    @GetMapping("/account")
+    public List<AccountEntity> getAllAccounts(){
+        return accountService.getAllAccounts();
+    }
+
+    @PutMapping("/account/new/{username}")
+    public AccountEntity updateAccount(@PathVariable("username") String username, 
+                                       @RequestBody AccountEntity entity)
+    {
+        return accountService.updateAccount(username, entity);
+    }
+
+    @DeleteMapping("/account/deletion/{username}")
+    public AccountEntity deleteAccount(@PathVariable("username") String username){
+        return accountService.deleteAccount(username);
+    }
+    /** END: ACCOUNT */
 }
--- src/main/java/com/daiduong/demo/service/AccountService.java
+++ src/main/java/com/daiduong/demo/service/AccountService.java
@@ -1,5 +1,145 @@
 package com.daiduong.demo.service;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import com.daiduong.demo.entity.AccountEntity;
+import com.daiduong.demo.exception.ApiRequestException;
+import com.daiduong.demo.repository.AccountRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AccountService {
     
+    @Autowired
+    private AccountRepository accountRepository;
+
+    // add account
+    public AccountEntity addAccount(AccountEntity account) {
+        String username = account.getUsername();
+        String password = account.getPassword();
+        String fullName = account.getFullName();
+        String phone = account.getPhone();
+        String address = account.getAddress();
+        String role = account.getRole();
+
+        if(username == null || username.trim().length() == 0){
+            throw new ApiRequestException("Username must not be null or empty");
+        }
+
+        Optional<AccountEntity> optional = accountRepository.findById(username);
+        if(optional.isPresent()){
+            throw new ApiRequestException("Username already used");
+        }
+
+        if(password == null || password.trim().length() < 6 || password.trim().length() > 20){
+            throw new ApiRequestException("Password's length must be between 6 and 20");
+        }
+
+        if(fullName == null || fullName.trim().length() == 0){
+            throw new ApiRequestException("Full name must not be null or empty");
+        }
+
+        if(phone == null || phone.trim().length() < 10 
+            || phone.trim().length() > 11 || !phone.matches("^[0-9]+$"))
+        {
+            throw new ApiRequestException("Phone must be number and length 10 or 11");
+        }
+
+        if(address == null || address.trim().length() == 0){
+            throw new ApiRequestException("Address must not be null or empty");
+        }
+
+        if(role == null || 
+            (!role.trim().equalsIgnoreCase("admin") && !role.trim().equalsIgnoreCase("customer")))
+        {
+            throw new ApiRequestException("Role must be admin or customer");
+        }
+        
+        LocalDate currentDate = LocalDate.now();
+        account.setCreateDate(currentDate);
+        account.setUpdateDate(currentDate);
+        account.setRole(role.toUpperCase());
+
+        return accountRepository.save(account);
+    }
+
+    // get all account
+    public List<AccountEntity> getAllAccounts(){
+        return accountRepository.findAll();
+    }
+
+    // update account
+    public AccountEntity updateAccount(String username, AccountEntity newAccount){
+        AccountEntity oldAccount = accountRepository.findById(username)
+                                   .orElseThrow(() -> new ApiRequestException(
+                                       "Username not found"
+                                    ));
+        String newPassword = newAccount.getPassword();
+        String newFullName = newAccount.getFullName();
+        String newPhone = newAccount.getPhone();
+        String newAddress = newAccount.getAddress();
+        String newRole = newAccount.getRole();
+        
+        String oldPassword = oldAccount.getPassword();
+        String oldFullName = oldAccount.getFullName();
+        String oldPhone = oldAccount.getPhone();
+        String oldAddress = oldAccount.getAddress();
+        String oldRole = oldAccount.getRole();
+
+        boolean isUpdate = false;
+
+        if(newPassword != null && newPassword.trim().length() > 0 && !newPassword.equals(oldPassword)){
+            oldAccount.setPassword(newPassword);
+            isUpdate = true;
+        }
+
+        if(newFullName != null && newFullName.trim().length() > 0 && !newFullName.equals(oldFullName)){
+            oldAccount.setFullName(newFullName);
+            isUpdate = true;
+        }
+
+        if(newPhone != null && (newPhone.trim().length() == 10 || newPhone.trim().length() == 11)
+            && newPhone.matches("^[0-9]+$") && !newPhone.equals(oldPhone))
+        {
+            oldAccount.setPhone(newPhone);
+            isUpdate = true;
+        }
+        
+        if(newAddress != null && newAddress.trim().length() > 0 && !newAddress.equals(oldAddress)){
+            oldAccount.setAddress(newAddress);
+            isUpdate = true;
+        }
+
+        if(newRole != null && newRole.trim().length() > 0 
+            && (newRole.trim().equalsIgnoreCase("admin") || newRole.trim().equalsIgnoreCase("customer"))
+            && !newRole.equals(oldRole))
+        {
+            oldAccount.setRole(newRole.toUpperCase());
+            isUpdate = true;
+        }    
+
+        if(isUpdate){
+            oldAccount.setUpdateDate(LocalDate.now());
+        }
+
+        return accountRepository.save(oldAccount);
+    }
+
+    // delete account
+    public AccountEntity deleteAccount(String username){
+        AccountEntity account = accountRepository.findById(username)
+                                .orElseThrow(() -> new ApiRequestException(
+                                    "Username not found"
+                                ));
+        if(account.isDeleted() == false){
+            account.setDeleted(true);
+            account.setUpdateDate(LocalDate.now());
+        }
+        
+        return accountRepository.save(account);
+    }
 }
